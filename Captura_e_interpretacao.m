47137925ad33b0b1b6fc52c91a366964f3d69026
felipekcamargo <felipekcamargo@hotmail.com>
2019-10-02 21:44:30 -0300

    Identação

--- Captura_e_interpretacao.m
+++ Captura_e_interpretacao.m
@@ -21,15 +21,14 @@ ch1 = data(:,1);
 DAQ_2 = timetable(seconds(timestamps),ch1);
 
 %% Plot Data
-
-%Plot para o domínio da frequência
+%Plot para o domï¿½nio da frequï¿½ncia
 control = 0;
 ampli=[];
 Xiz=[];
 saidaFinal=[];
 contador=1;
 while(contador<=60)
-    [Xiz,ampli,saidaFinal(contador)]=my_fft(DAQ_2.Variables,44100);
+    [Xiz,ampli,saidaFinal(contador)]=fourier(DAQ_2.Variables,44100);
     %pause(0.25)
     %fprintf('Worked!');
     [data, timestamps, starttime] = startForeground(s);
@@ -54,43 +53,42 @@ ylabel('Amplitude (V)')
 % Clear the session and channels, if any.
 clear s
 
-%% Função que passa para o dominio da frequencia
+%% Funcao que passa para o dominio da frequencia
 
-function [X,freq,saida] = my_fft(x,Fs)
-
-N = length(x);                      % variável N recebe o tamanho do vetor x
-k = 0:N-1;                          % k é um vetor que vai de zero até N menos 1
-T = N/Fs;                           % Vetor de tempo N dividido pela frequência de amostragem
-freq = k/T;
-X = fftn(x)/N;                      % X recebe a FFT normalizada do vetor x sobre N
-cutOff = ceil(N/2);                 % cutOff ajusta o eixo X
-X = X(1:cutOff);
-saida=0;
-%figure();
-frequency = freq(1:cutOff);
-amplitude = abs(X);
-%plot(frequency, amplitude);        % Plota a transformada de Fourier e o valor de X em módulo
-%title('Fast Fourier Transform');
-%xlabel('Frequency (Hz)');
-%ylabel('Amplitude');
-count = 0;
-j=397;
-while j<=407%407 é o id para as frequencias de 5900 e pouco a 6050 e poucos Hz
-            %entao ele verifica se existe valores numa certa amplitude para
-            %essas frequencias no if abaixo.
-    %amplitude(j,1)
-    if amplitude(j,1)>=0.000001 %verificar montagem do vetor
-        saida=amplitude(j,1);%1;
+function [X,freq,saida] = fourier(x,Fs)
+    N = length(x);                      % variï¿½vel N recebe o tamanho do vetor x
+    k = 0:N-1;                          % k ï¿½ um vetor que vai de zero atï¿½ N menos 1
+    T = N/Fs;                           % Vetor de tempo N dividido pela frequï¿½ncia de amostragem
+    freq = k/T;
+    X = fftn(x)/N;                      % X recebe a FFT normalizada do vetor x sobre N
+    cutOff = ceil(N/2);                 % cutOff ajusta o eixo X
+    X = X(1:cutOff);
+    saida=0;
+    %figure();
+    frequency = freq(1:cutOff);
+    amplitude = abs(X);
+    %plot(frequency, amplitude);        % Plota a transformada de Fourier e o valor de X em mï¿½dulo
+    %title('Fast Fourier Transform');
+    %xlabel('Frequency (Hz)');
+    %ylabel('Amplitude');
+    count = 0;
+    j=397;
+    while j<=407%407 ï¿½ o id para as frequencias de 5900 e pouco a 6050 e poucos Hz
+                %entao ele verifica se existe valores numa certa amplitude para
+                %essas frequencias no if abaixo.
+        %amplitude(j,1)
+        if amplitude(j,1)>=0.000001 %verificar montagem do vetor
+            saida=amplitude(j,1);%1;
+        end
+        j=j+1;
     end
-    j=j+1;
-end
-% while count < 200
-%     if amplitude(count+6000) > 1.5e-07
-%         fprintf('Worked!');
-%         break;
-%     end
-%     count = count + 1;
-% end
+    % while count < 200
+    %     if amplitude(count+6000) > 1.5e-07
+    %         fprintf('Worked!');
+    %         break;
+    %     end
+    %     count = count + 1;
+    % end
 
 end
 
